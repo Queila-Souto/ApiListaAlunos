86f2074fb5f8323e32a35a006f382bf120853906
QUEILA SOUTO <73967682+Queila-Souto@users.noreply.github.com>
2025-05-24 20:27:22 -0300

    "Configuração de segurança aprimorada: adicionados endpoints com requisitos de autenticação, filtros de autorização e codificador de senha."

--- src/main/java/com/listaVip/cadastro/repository/SecurityConfig.java
+++ src/main/java/com/listaVip/cadastro/repository/SecurityConfig.java
@@ -1,7 +1,18 @@
 package com.listaVip.cadastro.repository;
 
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -10,4 +21,45 @@ public class SecurityConfig {
             "/usuarios/login", //url que usaremos para fazer login
             "/usuario", //url que usaremos para criar um usuário
     };
+    // Endpoints que requerem autenticação para serem acessados
+    public static final String [] ENDPOINTS_WITH_AUTHENTICATION_REQUIRED = {
+            "/users/test"
+    };
+
+    // Endpoints que só podem ser acessador por usuários com permissão de cliente
+    public static final String [] ENDPOINTS_CUSTOMER = {
+            "/users/test/customer"
+    };
+
+    // Endpoints que só podem ser acessador por usuários com permissão de administrador
+    public static final String [] ENDPOINTS_ADMIN = {
+            "/users/test/administrator"
+    };
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        UserAuthFilter userAuthFilter = new UserAuthFilter();
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).permitAll()
+                        .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_REQUIRED).authenticated()
+                        .requestMatchers(ENDPOINTS_ADMIN).hasRole("ADMINISTRATOR")
+                        .requestMatchers(ENDPOINTS_CUSTOMER).hasRole("CUSTOMER")
+                        .anyRequest().denyAll())
+                .addFilterBefore(userAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
+
